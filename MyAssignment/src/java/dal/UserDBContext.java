f8ae70d41a61f0bd3f62bbf3cf5197fdfc895441
PhucHoangchu <phucchhe181428@fpt.edu.vn>
2025-10-25 17:54:30 +0700

    Update UserDBContext.java
    done CRUD for User

--- MyAssignment/src/java/dal/UserDBContext.java
+++ MyAssignment/src/java/dal/UserDBContext.java
@@ -58,26 +58,124 @@ public class UserDBContext extends DBContext<User> {
 
     @Override
     public ArrayList<User> list() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ArrayList<User> users = new ArrayList<>();
+        try {
+            String sql = """
+                         SELECT
+                         u.uid,
+                         u.username,
+                         u.displayname,
+                         e.eid,
+                         e.ename
+                         FROM [User] u 
+                         LEFT JOIN [Enrollment] en ON u.[uid] = en.[uid] AND en.active = 1
+                         LEFT JOIN [Employee] e ON e.eid = en.eid""";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt("uid"));
+                u.setUsername(rs.getString("username"));
+                u.setDisplayname(rs.getString("displayname"));
+
+                if (rs.getObject("eid") != null) {
+                    Employee e = new Employee();
+                    e.setId(rs.getInt("eid"));
+                    e.setName(rs.getString("ename"));
+                    u.setEmployee(e);
+                }
+                users.add(u);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeConnection();
+        }
+        return users;
     }
 
     @Override
     public User get(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            String sql = """
+                         SELECT
+                         u.uid,
+                         u.username,
+                         u.displayname,
+                         e.eid,
+                         e.ename
+                         FROM [User] u 
+                         LEFT JOIN [Enrollment] en ON u.[uid] = en.[uid] AND en.active = 1
+                         LEFT JOIN [Employee] e ON e.eid = en.eid
+                         WHERE u.uid = ?""";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt("uid"));
+                u.setUsername(rs.getString("username"));
+                u.setDisplayname(rs.getString("displayname"));
+
+                if (rs.getObject("eid") != null) {
+                    Employee e = new Employee();
+                    e.setId(rs.getInt("eid"));
+                    e.setName(rs.getString("ename"));
+                    u.setEmployee(e);
+                }
+                return u;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeConnection();
+        }
+        return null;
     }
 
     @Override
     public void insert(User model) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            String sql = "INSERT INTO [User] (uid, username, [password], displayname) VALUES (?, ?, ?, ?)";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, model.getId());
+            stm.setString(2, model.getUsername());
+            stm.setString(3, "123"); // Password nên được xử lý riêng, tạm hardcode
+            stm.setString(4, model.getDisplayname());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeConnection();
+        }
     }
 
     @Override
     public void update(User model) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            String sql = "UPDATE [User] SET displayname = ? WHERE uid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, model.getDisplayname());
+            stm.setInt(2, model.getId());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeConnection();
+        }
     }
 
     @Override
     public void delete(User model) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            String sql = "DELETE FROM [User] WHERE uid = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, model.getId());
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeConnection();
+        }
     }
 }
