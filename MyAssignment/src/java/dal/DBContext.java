1ea30b6e2a0c6e6df23561340e45c2414a830158
PhucHoangchu <phucchhe181428@fpt.edu.vn>
2025-11-02 06:03:28 +0700

    DBContext.java
    add sendStringParametersAsUnicode to SQL Server connection string

--- MyAssignment/src/java/dal/DBContext.java
+++ MyAssignment/src/java/dal/DBContext.java
@@ -4,7 +4,6 @@
  */
 package dal;
 
-import com.sun.jdi.connect.spi.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -25,7 +24,7 @@ public abstract class DBContext<T extends BaseModel> {
         try {
             String user = "phucch";
             String pass = "123";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=FALL25_Assignment;encrypt=true;trustServerCertificate=true;";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=FALL25_Assignment;encrypt=true;trustServerCertificate=true;sendStringParametersAsUnicode=true;";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
